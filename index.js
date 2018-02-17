bc7ca129a8273caccc92e18aca933cc3e1df7af9
Justin Tooley <jstooley823@gmail.com>
2018-02-17 02:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function fuzzyMatch(drivers, name) {
 };
 
 function matchName(drivers, name){
-  return drivers.filter(function (driver) { return driver === name;});
+  return drivers.filter(function (driver) { return driver.name === name;});
 };
